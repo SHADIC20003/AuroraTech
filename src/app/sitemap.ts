b818a78c2816e2eb6d64814ee7bc00732ec9e1f4
Ali Tamer <alitamer82.at@gmail.com>
2024-07-27 18:16:14 +0300

    added www in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,7 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const BASE_URL = 'https://auroratech.me'
+    const BASE_WITH_WWW = 'https://www.auroratech.me'
 
     return [
         {
@@ -26,6 +27,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
         },
         {
             url: `${BASE_URL}/services`,
+            lastModified: new Date(),
+            priority: 1,
+        },
+        {
+            url: `${BASE_WITH_WWW}/`,
+            lastModified: new Date(),
+            priority: 1,
+        },
+        {
+            url: `${BASE_WITH_WWW}/our-work`,
+            lastModified: new Date(),
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_WITH_WWW}/contact`,
+            lastModified: new Date(),
+            priority: 1,
+        },
+        {
+            url: `${BASE_WITH_WWW}/about-us`,
+            lastModified: new Date(),
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_WITH_WWW}/services`,
             lastModified: new Date(),
             priority: 1,
         },
