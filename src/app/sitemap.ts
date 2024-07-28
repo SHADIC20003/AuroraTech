19b8210df4a51a2428ad3cb7eb21d4027216ce39
Ali Tamer <alitamer82.at@gmail.com>
2024-07-28 19:29:35 +0300

    changed sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,11 @@
+import { prisma } from '@/server/db'
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const BASE_URL = 'https://auroratech.me'
     const BASE_WITH_WWW = 'https://www.auroratech.me'
+
+    const blogs = await prisma.blog.findMany()
 
     return [
         {
@@ -65,5 +68,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
             lastModified: new Date(),
             priority: 0.9,
         },
+        ...blogs.map((blog) => ({
+            url: `${BASE_URL}/blog/${blog.id}`,
+            lastModified: new Date(),
+            priority: 0.8,
+        })),
+        ...blogs.map((blog) => ({
+            url: `${BASE_WITH_WWW}/blog/${blog.id}`,
+            lastModified: new Date(),
+            priority: 0.8,
+        })),
     ]
 }
